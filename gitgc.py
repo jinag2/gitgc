3869d63e0476e91506c50f2d57225855081b1473
Albert Chiang <jinag2@gmail.com>
2018-07-13 11:04:40 +0800

    Refactoring the code

--- gitgc.py
+++ gitgc.py
@@ -10,7 +10,6 @@ def main():
 	if len(sys.argv) == 1 or sys.argv[1] in ('-h', '"-H', '/h', '/H', '--help', '--HELP', '?', '/?'):
 		print('usage: py gitgc.py <path>')
 		sys.exit()
-		return
 
 	target_path = sys.argv[1]
 	if not os.path.isdir(target_path):
@@ -19,58 +18,29 @@ def main():
 
 	work_dir = os.getcwd()
 
-# 	if sys.argv[1] == ".":
-# 	    target_path = os.getcwd()
-# 	elif sys.argv[1] == "..":
-# 		target_path = os.path.dirname(os.getcwd())
-
 	target_path = os.path.abspath(sys.argv[1])  # converted to absolute path if the path is relative
-	git_folder_list = []
-	get_git_folders(target_path, git_folder_list, True)
+	git_folders = get_git_folders(target_path, True)
 
-	print_git_folders(git_folder_list)
-	print('Total git folder: ' + str(len(git_folder_list)) + '\n')
+	print_git_folders(git_folders)
+	print('Total git folder: ' + str(len(git_folders)) + '\n')
 
-	exec_git_gc(git_folder_list)
+	exec_git_gc(git_folders)
 	os.chdir(work_dir)
 
 
-def get_git_folders(cur_path, git_folder_list, print_folder):
+def get_git_folders(cur_path, print_folder):
 	if print_folder:
 		print(cur_path)
 
 	os.chdir(cur_path)
-	folder_list = glob.glob("**/.git", recursive=True)
+	folders = glob.glob("**/.git", recursive=True)
 
-	for elem in folder_list:
-		git_folder_list.append(os.path.dirname(os.path.abspath(elem)))
+	git_folders = []
+	for elem in folders:
+		git_folders.append(os.path.dirname(os.path.abspath(elem)))
 
+	return git_folders
 
-# old version of get_git_folders
-'''
-def get_git_folders(cur_path, git_folder_list, print_folder):
-	if print_folder:
-		print(cur_path)
-
-	item_list = os.listdir(cur_path)
-
-	is_git = False
-	sub_folder_list = []
-	for elem in item_list:
-		fullpath = os.path.join(cur_path, elem)
-		if elem == '.git':
-			is_git = True
-
-		if os.path.isdir(fullpath):
-			sub_folder_list.append(fullpath)
-
-	if is_git:
-		git_folder_list.append(cur_path)
-		return
-
-	for elem in sub_folder_list:
-		get_git_folders(elem, git_folder_list, False)
-'''
 
 def print_git_folders(git_folder_list):
 	print(' git folder list '.center(31, '='))
@@ -90,8 +60,6 @@ def exec_git_gc(git_folder_list):
 
 def exec_cmd(args):
 	output = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
-#	print(type(output))
-#	print(output)
 	stdout_list = output.decode("ascii").split('\n')
 	for elem in stdout_list:
 		print(elem)
